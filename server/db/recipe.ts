3802b9a7ca758e22ec3a3f9e5f872b49fe8d09f4
Artur <artur.lindstrom@gmail.com>
2022-05-10 09:41:41 +0200

    fixed search feature

--- server/db/recipe.ts
+++ server/db/recipe.ts
@@ -4,7 +4,7 @@ export const getRecipes = async () => {
     const recipes = await RecipeModel.find();
     return recipes;
 }
-export const getRecipesBySearch = async (search: string) => {
+export const getRecipesBySearch = async (search: any) => {
     const recipes = await RecipeModel.find({
         $or: [
             { title: { $regex: search, $options: 'i' } }
