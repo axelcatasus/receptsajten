cbcb8549281364b5b62f97c8b9878f5901a8903c
axelcatasus <axel.catasus@gmail.com>
2022-05-19 14:24:52 +0200

    dnsia

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SingleRecipe from './features/recipes/SingleRecipe';
 import CategoryView from './features/categories/CategoryView';
 import { store } from './app/store'
